7902011509ca98dbbf6a6f018fe868c0d367296e
gingrspacecadet <jackrawlings2011@gmail.com>
2025-07-16 20:51:51 +0100

    refactor: clearer backend file structure

--- functions/api/utils.js
+++ functions/api/utils.js
@@ -23,6 +23,6 @@ export async function hashPassword(pw) {
     const enc = new TextEncoder().encode(pw);
     const hash = await crypto.subtle.digest("SHA-256", enc);
     return Array.from(new Uint8Array(hash))
-      .map((b) => b.toString(16).padStart(2, "0"))
-      .join("");
+        .map((b) => b.toString(16).padStart(2, "0"))
+        .join("");
 }
